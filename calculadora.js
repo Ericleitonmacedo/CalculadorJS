3a2f07bb391df2f80131b85db156f00fd0f80013
Ericleiton Macedo <ericleitonmacedo@gmail.com>
2020-09-23 22:25:34 -0300

    Calculadora feita em HTML, CSS e JavaScript

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+var valor;
+var resultado;
+function botao(num){
+    valor = document.calculadora.visor.value += num;   
+}
+
+function reset(){
+    document.calculadora.visor.value = '';
+}
+
+function calcula(){
+    resultado = eval(valor);
+    document.calculadora.visor.value = resultado
+}
